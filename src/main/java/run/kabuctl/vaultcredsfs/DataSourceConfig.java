358874d0b2bd3fdbc07716b6e006cd5b5b6a0cea
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-30 13:31:25 +0900

    consul config

--- src/main/java/run/kabuctl/vaultcredsfs/DataSourceConfig.java
+++ src/main/java/run/kabuctl/vaultcredsfs/DataSourceConfig.java
@@ -9,26 +9,25 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-@Component
-@Configuration
+//@Configuration
 public class DataSourceConfig {
 
-    @Bean
-    public DataSource getDataSource() throws Exception {
-
-        MysqlCreds getMysqlCreds = new MysqlCreds();
-        System.out.println("Username: " + getMysqlCreds.getCreds()[0]);
-        System.out.println("Password: " + getMysqlCreds.getCreds()[1]);
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/handson?useSSL=false");
-        config.setDriverClassName("com.mysql.jdbc.Driver");
-        config.setUsername(getMysqlCreds.getCreds()[0]);
-        config.setPassword(getMysqlCreds.getCreds()[1]);
-        config.setMaxLifetime(30000);
-        config.setConnectionTimeout(300);
-        config.setConnectionInitSql("SELECT 1");
-        HikariDataSource dataSource = new HikariDataSource(config);
-
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource getDataSource() throws Exception {
+//
+//        MysqlCreds getMysqlCreds = new MysqlCreds();
+//        System.out.println("Username: " + getMysqlCreds.getCreds()[0]);
+//        System.out.println("Password: " + getMysqlCreds.getCreds()[1]);
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/handson?useSSL=false");
+//        config.setDriverClassName("com.mysql.jdbc.Driver");
+//        config.setUsername(getMysqlCreds.getCreds()[0]);
+//        config.setPassword(getMysqlCreds.getCreds()[1]);
+//        config.setMaxLifetime(30000);
+//        config.setConnectionTimeout(300);
+//        config.setConnectionInitSql("SELECT 1");
+//        HikariDataSource dataSource = new HikariDataSource(config);
+//
+//        return dataSource;
+//    }
 }
---
+++ update-config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+vault read -format=json database/creds/role-handson > mysql-creds.json
+
+sed  "s|USERNAME|`cat mysql-creds.json | jq -r '.data.username'`|g" application-template.yml > application.yml
+sed -i -e "s|PASSWORD|`cat mysql-creds.json | jq -r '.data.password'`|g" application.yml
+
+
+consul kv put config/application/data @application.yml
+
+rm application.yml-e
